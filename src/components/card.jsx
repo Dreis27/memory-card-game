8d4c82f9ff4e83dd8a1926327ed8ac3e32232c65
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-10-02 19:11:00 +0200

    edit card component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,24 +1,35 @@
+import { useEffect, useState } from "react";
 import Tilt from 'react-parallax-tilt';
 
-export default function Card ({character, isFlipped, handleClick}) {
+export default function Card ({character, handleClick, cardsShowing}) {
+
+    const ANIMATION_TIME = 850;
+    const [interactable, setInteractable] = useState(false);
+  
+    useEffect(() => {
+      setTimeout(() => setInteractable(true), ANIMATION_TIME);
+    }, []);
+
     return (
-        <div className={isFlipped ? "card flipped" : "card"}
-                onClick={() => {handleClick(character)}}>
+
                 <Tilt
                     glareEnable={true}
                     glareMaxOpacity={0.6}
                     glareColor="#ffffff"
                     glarePosition="bottom"
                     glareBorderRadius="20px"
-                    className='tilt'>
-                        <div className='card-face'>
-                            <div className='card-image'
-                                style={{backgroundImage: `url(${character.src})`}}>
+                    className={`card-container ${cardsShowing ? "front" : "back"} ${
+                        cardsShowing && interactable ? undefined : "pointer-events-none"
+                      }`}>
+                        <div className="card-container">
+                            <div className='card-face'>
+                                <div className='card-image' onClick={handleClick}
+                                    style={{backgroundImage: `url(${character.src})`}}>
+                                </div>
+                                <div className='card-name'>{character.name}</div>
                             </div>
-                            <div className='card-name'>{character.name}</div>
+                            <div className='card-back'></div>
                         </div>
-                        <div className='card-back'></div>
                 </Tilt>
-        </div>
     ) 
 }
